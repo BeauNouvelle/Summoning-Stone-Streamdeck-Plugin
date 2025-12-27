a4f8421b67c929be7172e406cab3cd26bb71f4f9
Beau Nouvelle <beau@getswifty.dev>
2025-12-27 18:21:19 +1100

    Trigger SFX play endpoint on key press

--- src/actions/increment-counter.ts
+++ src/actions/increment-counter.ts
@@ -1,11 +1,19 @@
-import { action, SingletonAction } from "@elgato/streamdeck";
+import { action, KeyDownEvent, SingletonAction } from "@elgato/streamdeck";
 
 /**
  * An example action class.
  */
 @action({ UUID: "com.beau-nouvelle.summoning-stone---ttrpg-sfx-soundboard-music--ambience.increment" })
 export class IncrementCounter extends SingletonAction<CounterSettings> {
-	// Action logic will be added in a future iteration.
+	override async onKeyDown(ev: KeyDownEvent<CounterSettings>): Promise<void> {
+		const sfxName = ev.payload.settings.sfxName;
+		if (!sfxName) {
+			return;
+		}
+
+		const encodedName = encodeURIComponent(sfxName);
+		await fetch(`http://127.0.0.1:7123/sfx/${encodedName}/play`, { method: "POST" });
+	}
 }
 
 /**
