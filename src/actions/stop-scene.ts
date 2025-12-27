0442c98db06b10767bd6158a20b01f8ed784d975
Beau Nouvelle <beau@getswifty.dev>
2025-12-28 00:30:59 +1100

    Shorten plugin UUID and fix action typing

--- src/actions/stop-scene.ts
+++ src/actions/stop-scene.ts
@@ -1,6 +1,6 @@
 import { action, KeyDownEvent, SingletonAction } from "@elgato/streamdeck";
 
-@action({ UUID: "com.beau-nouvelle.summoning-stone---ttrpg-sfx-soundboard-music--ambience.stop-scene" })
+@action({ UUID: "com.beaunouvelle.summoning-stone.stop-scene" })
 export class StopScene extends SingletonAction {
 	override async onKeyDown(_ev: KeyDownEvent): Promise<void> {
 		await fetch("http://127.0.0.1:7123/scenes/stop", { method: "POST" });
